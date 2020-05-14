096db64457105d03bb8c330c109ea3fd8ca2da9c
a4johnny <40929308+a4johnny@users.noreply.github.com>
2020-05-14 18:47:31 +0800

    new crawler edit by jimmy 2

---
+++ CCU-Crawler2/CCU-Crawler/Models/DepartmentModels.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CCU_Crawler.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CollegeId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
