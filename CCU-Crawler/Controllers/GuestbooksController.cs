38a35eb32d1c67a4f521b2ecb75c6c19d0be6f2b
a4johnny <40929308+a4johnny@users.noreply.github.com>
2020-05-15 21:36:09 +0800

    add conplain and list

---
+++ CCU-Crawler/Controllers/GuestbooksController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CCU_Crawler.Models;
+
+namespace CCU_Crawler.Controllers
+{
+    public class GuestbooksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Guestbooks
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+
+        // GET: Guestbooks/Details/5
+
+        public ActionResult List(int? id)
+        {   
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }  
+            Course course = db.Courses.Find(id);
+            TempData["name"] = course.Name;
+            TempData["teacher"] = course.Teacher;
+            TempData["remark"] = course.Remark;
+     
+            return View(db.Guestbooks.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult List(int score, string content)
+        {
+            Guestbook guestbook = new Guestbook();
+            guestbook.Score = score;
+            guestbook.Content = content;
+            db.Guestbooks.Add(guestbook);
+            db.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult Conplain(int score, string content)
+        {
+            Guestbook guestbook = new Guestbook();
+            guestbook.Score = score;
+            guestbook.Content = content;
+            db.Guestbooks.Add(guestbook);
+            db.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // GET: Guestbooks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Guestbooks/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Score,Content")] Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guestbooks.Add(guestbook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(guestbook);
+        }
+
+        // GET: Guestbooks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // POST: Guestbooks/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Score,Content")] Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guestbook).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guestbook);
+        }
+
+        // GET: Guestbooks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // POST: Guestbooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            db.Guestbooks.Remove(guestbook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CCU-Crawler/Models/GuestbookModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CCU_Crawler.Models
+{
+    public class Guestbook
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Content { get; set; }
+    }
+}
