88933c4aac818766c6e983f5c90b50662caa8e08
Jimmy Kao <skzeratal@gmail.com>
2020-06-02 00:26:19 +0800

    course search 2

--- CCU-Crawler/Controllers/CourseSearchController.cs
+++ CCU-Crawler/Controllers/CourseSearchController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using CCU_Crawler.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace CCU_Crawler.Controllers
 {
@@ -15,164 +13,109 @@ namespace CCU_Crawler.Controllers
         private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: CourseSearch
-        public ActionResult Index()
+        public ActionResult Index(SearchCourse searchCourse)
         {
-            return View(db.Courses.ToList());
-        }
-
-        public ActionResult Search(string departmentId, string grade, string name, string teacher)
-        {
-            List<Course> courses = new List<Course>();
-
-            if (departmentId != string.Empty)
+            if (searchCourse is object)
             {
-                var integerDepartmentId = int.Parse(departmentId);
-                courses = db.Courses.Where(course => course.DepartmentId == integerDepartmentId).ToList();
-            }
-
-            if (grade != string.Empty)
-            {
-                var integerGrade = int.Parse(grade);
-                if (courses.Count == 0)
+                var courses = new List<Course>();
+                if (!searchCourse.DepartmentName.IsNullOrWhiteSpace())
                 {
-                    courses = db.Courses.Where(course => course.Grade == integerGrade).ToList();
+                    var integerDepartmentId = db.Departments.Where(department => department.Name.Contains(searchCourse.DepartmentName)).FirstOrDefault().Id;
+                    courses = db.Courses.Where(course => course.DepartmentId == integerDepartmentId).ToList();
+                    Debug.WriteLine(courses.Count);
                 }
                 else
                 {
-                    courses = courses.Where(course => course.Grade == integerGrade).ToList();
+                    courses = db.Courses.ToList();
                 }
+                if (!searchCourse.Grade.IsNullOrWhiteSpace() && searchCourse.Grade != string.Empty)
+                {
+                    var integerGrade = int.Parse(searchCourse.Grade);
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Grade == integerGrade).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Grade == integerGrade).ToList();
+                    }
+                }
+                if (!searchCourse.Name.IsNullOrWhiteSpace() && searchCourse.Name != string.Empty)
+                {
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Name.Contains(searchCourse.Name)).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Name.Contains(searchCourse.Name)).ToList();
+                    }
+                }
+                if (!searchCourse.Teacher.IsNullOrWhiteSpace() && searchCourse.Teacher != string.Empty)
+                {
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Teacher.Contains(searchCourse.Teacher)).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Teacher.Contains(searchCourse.Teacher)).ToList();
+                    }
+                }
+                var coursesToView = (from course in courses
+                                     join department in db.Departments on course.DepartmentId equals department.Id
+                                     select new CourseToView()
+                                     {
+                                         Id = course.Id,
+                                         DepartmentName = department.Name,
+                                         Grade = course.Grade,
+                                         CourseName = course.Name,
+                                         Teacher = course.Teacher,
+                                         Duration = course.Duration,
+                                         Credit = course.Credit,
+                                         Type = course.Type,
+                                         Time = course.Time,
+                                         Location = course.Location,
+                                         Limit = course.Limit,
+                                         Url = course.Url,
+                                         Remark = course.Remark
+                                     }).ToList();
+                return PartialView(coursesToView);
             }
-
-            if (name != string.Empty)
+            else
             {
-                System.Diagnostics.Debug.WriteLine(true);
-                if (courses.Count == 0)
-                {
-                    courses = db.Courses.Where(course => course.Name.Contains(name)).ToList();
-                }
-                else
-                {
-                    courses = courses.Where(course => course.Name.Contains(name)).ToList();
-                }
+                var courses = db.Courses.ToList();
+                var coursesToView = (from course in courses
+                                     join department in db.Departments on course.DepartmentId equals department.Id
+                                     select new CourseToView()
+                                     {
+                                         Id = course.Id,
+                                         DepartmentName = department.Name,
+                                         Grade = course.Grade,
+                                         CourseName = course.Name,
+                                         Teacher = course.Teacher,
+                                         Duration = course.Duration,
+                                         Credit = course.Credit,
+                                         Type = course.Type,
+                                         Time = course.Time,
+                                         Location = course.Location,
+                                         Limit = course.Limit,
+                                         Url = course.Url,
+                                         Remark = course.Remark
+                                     }).ToList();
+                return PartialView(coursesToView);
             }
-
-            if (teacher != string.Empty)
-            {
-                if (courses.Count == 0)
-                {
-                    courses = db.Courses.Where(course => course.Teacher.Contains(teacher)).ToList();
-                }
-                else
-                {
-                    courses = courses.Where(course => course.Teacher.Contains(teacher)).ToList();
-                }
-            }
-
-            return View(courses);
         }
 
-        // GET: CourseSearch/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Course course = db.Courses.Find(id);
-            if (course == null)
-            {
-                return HttpNotFound();
-            }
-            return View(course);
-        }
-
-        // GET: CourseSearch/Create
-        public ActionResult Create()
+        public ActionResult Search()
         {
             return View();
         }
 
-        // POST: CourseSearch/Create
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,DepartmentId,Grade,Number,Class,Name,Teacher,Duration,Credit,Type,Time,Location,Limit,Url,Remark")] Course course)
+        public ActionResult Search([Bind(Include = "DepartmentName,Grade,Name,Teacher")] SearchCourse searchCourse)
         {
-            if (ModelState.IsValid)
-            {
-                db.Courses.Add(course);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(course);
-        }
-
-        // GET: CourseSearch/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Course course = db.Courses.Find(id);
-            if (course == null)
-            {
-                return HttpNotFound();
-            }
-            return View(course);
-        }
-
-        // POST: CourseSearch/Edit/5
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,DepartmentId,Grade,Number,Class,Name,Teacher,Duration,Credit,Type,Time,Location,Limit,Url,Remark")] Course course)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(course).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(course);
-        }
-
-        // GET: CourseSearch/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Course course = db.Courses.Find(id);
-            if (course == null)
-            {
-                return HttpNotFound();
-            }
-            return View(course);
-        }
-
-        // POST: CourseSearch/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Course course = db.Courses.Find(id);
-            db.Courses.Remove(course);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return View(searchCourse);
         }
     }
 }
