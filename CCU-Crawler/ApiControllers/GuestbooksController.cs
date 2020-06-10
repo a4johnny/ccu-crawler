caec6fa422978caf9652a5244b6a3f398acfbb01
a4johnny <40929308+a4johnny@users.noreply.github.com>
2020-06-10 18:16:58 +0800

    another reset again

---
+++ CCU-Crawler/ApiControllers/GuestbooksController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CCU_Crawler.Models;
+
+namespace CCU_Crawler.ApiControllers
+{
+    public class GuestbooksController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Guestbooks
+        public IQueryable<Guestbook> GetGuestbooks()
+        {
+            return db.Guestbooks;
+        }
+
+        // GET: api/Guestbooks/5
+        [ResponseType(typeof(Guestbook))]
+        public IHttpActionResult GetGuestbook(int id)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(guestbook);
+        }
+
+        // PUT: api/Guestbooks/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutGuestbook(int id, Guestbook guestbook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != guestbook.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(guestbook).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GuestbookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Guestbooks
+        [ResponseType(typeof(Guestbook))]
+        public IHttpActionResult PostGuestbook(Guestbook guestbook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Guestbooks.Add(guestbook);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = guestbook.Id }, guestbook);
+        }
+
+        // DELETE: api/Guestbooks/5
+        [ResponseType(typeof(Guestbook))]
+        public IHttpActionResult DeleteGuestbook(int id)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return NotFound();
+            }
+
+            db.Guestbooks.Remove(guestbook);
+            db.SaveChanges();
+
+            return Ok(guestbook);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GuestbookExists(int id)
+        {
+            return db.Guestbooks.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ CCU-Crawler/Controllers/CourseSearchController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+using CCU_Crawler.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace CCU_Crawler.Controllers
+{
+    public class CourseSearchController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CourseSearch
+        public ActionResult Index(SearchCourse searchCourse)
+        {
+            if (searchCourse is object)
+            {
+                var courses = new List<Course>();
+                if (!searchCourse.DepartmentName.IsNullOrWhiteSpace())
+                {
+                    var currentDepartment = db.Departments.Where(department => department.Name.Contains(searchCourse.DepartmentName)).FirstOrDefault();
+                    if (currentDepartment is object)
+                    {
+                        var integerDepartmentId = db.Departments.Where(department => department.Name.Contains(searchCourse.DepartmentName)).FirstOrDefault().Id;
+                        courses = db.Courses.Where(course => course.DepartmentId == integerDepartmentId).ToList();
+                        Debug.WriteLine(courses.Count);
+                    }
+                }
+                else
+                {
+                    courses = db.Courses.ToList();
+                }
+                if (!searchCourse.Grade.IsNullOrWhiteSpace() && searchCourse.Grade != string.Empty)
+                {
+                    var integerGrade = int.Parse(searchCourse.Grade);
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Grade == integerGrade).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Grade == integerGrade).ToList();
+                    }
+                }
+                if (!searchCourse.Name.IsNullOrWhiteSpace() && searchCourse.Name != string.Empty)
+                {
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Name.Contains(searchCourse.Name)).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Name.Contains(searchCourse.Name)).ToList();
+                    }
+                }
+                if (!searchCourse.Teacher.IsNullOrWhiteSpace() && searchCourse.Teacher != string.Empty)
+                {
+                    if (courses.Count == 0)
+                    {
+                        courses = db.Courses.Where(course => course.Teacher.Contains(searchCourse.Teacher)).ToList();
+                    }
+                    else
+                    {
+                        courses = courses.Where(course => course.Teacher.Contains(searchCourse.Teacher)).ToList();
+                    }
+                }
+                var coursesToView = (from course in courses
+                                     join department in db.Departments on course.DepartmentId equals department.Id
+                                     select new CourseToView()
+                                     {
+                                         Id = course.Id,
+                                         DepartmentName = department.Name,
+                                         Grade = course.Grade,
+                                         CourseName = course.Name,
+                                         Teacher = course.Teacher,
+                                         Duration = course.Duration,
+                                         Credit = course.Credit,
+                                         Type = course.Type,
+                                         Time = course.Time,
+                                         Location = course.Location,
+                                         Limit = course.Limit,
+                                         Url = course.Url,
+                                         Remark = course.Remark
+                                     }).ToList();
+                return PartialView(coursesToView);
+            }
+            else
+            {
+                var courses = db.Courses.ToList();
+                var coursesToView = (from course in courses
+                                     join department in db.Departments on course.DepartmentId equals department.Id
+                                     select new CourseToView()
+                                     {
+                                         Id = course.Id,
+                                         DepartmentName = department.Name,
+                                         Grade = course.Grade,
+                                         CourseName = course.Name,
+                                         Teacher = course.Teacher,
+                                         Duration = course.Duration,
+                                         Credit = course.Credit,
+                                         Type = course.Type,
+                                         Time = course.Time,
+                                         Location = course.Location,
+                                         Limit = course.Limit,
+                                         Url = course.Url,
+                                         Remark = course.Remark
+                                     }).ToList();
+                return PartialView(coursesToView);
+            }
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search([Bind(Include = "DepartmentName,Grade,Name,Teacher")] SearchCourse searchCourse)
+        {
+            return RedirectToAction("Index", searchCourse);
+        }
+    }
+}
