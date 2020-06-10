e3d99ef7a7fa11f030e24c951add38073afe715b
a4johnny <40929308+a4johnny@users.noreply.github.com>
2020-06-10 16:39:01 +0800

    chart.js

---
+++ CCU-Crawler/Content/guestbooklist.js
@@ -0,0 +1,50 @@
+var guestbook = /** @class */ (function () {
+    function guestbook() {
+    }
+    return guestbook;
+}());
+var guestbooklistModel = /** @class */ (function () {
+    function guestbooklistModel() {
+        this.Guestbooklist = [];
+    }
+    return guestbooklistModel;
+}());
+var guestbooklistView = /** @class */ (function () {
+    function guestbooklistView(m) {
+        this.Model = m;
+        this.CreateView();
+        this.Update();
+    }
+    guestbooklistView.prototype.CreateView = function () {
+    };
+    guestbooklistView.prototype.Update = function () {
+        //   console.log(e[0].Score);
+        this.Model.Guestbooklist.forEach(function (value, index, Array) {
+            console.log('index[' + index + ']:' + value.Score);
+        });
+    };
+    return guestbooklistView;
+}());
+var guestbooklistController = /** @class */ (function () {
+    function guestbooklistController() {
+        this.Model = new guestbooklistModel;
+        this.View = new guestbooklistView(this.Model);
+        this.SubscribeEvents();
+        this.Main();
+    }
+    guestbooklistController.prototype.SubscribeEvents = function () {
+    };
+    guestbooklistController.prototype.Main = function () {
+        var _this = this;
+        $.get('/api/Guestbooks/', function (e) { return _this.GetGuestbooks(e); });
+    };
+    guestbooklistController.prototype.GetGuestbooks = function (e) {
+        this.Model.Guestbooklist = e;
+        this.View.Update();
+    };
+    return guestbooklistController;
+}());
+$().ready(function () {
+    var app = new guestbooklistController();
+});
+//# sourceMappingURL=guestbooklist.js.map
